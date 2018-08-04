b8f4f7ce09419d234082dcac42f17ece14f38f2c
vhyz <32564682@qq.com>
2018-08-05 00:09:16 +0800

    list

--- LightSTL/list.hpp
+++ LightSTL/list.hpp
@@ -11,13 +11,6 @@ namespace detail {
 
 template<class T,class Allocator=LightSTL::allocator<T>>
 class list {
-private:
-	template<class T>
-	class list_node {
-		list_node* prev;
-		list_node* next;
-		T data;
-	};
 
 public:
 	//typedefs
@@ -34,7 +27,41 @@ public:
 	using reverse_iterator = LightSTL::reverse_iterator<iterator>;
 	using const_reverse_iterator = LightSTL::reverse_iterator<const_iterator>;
 
+private:
+	//node
+	template<class T>
+	struct node {
+		node* prev;
+		node* next;
+		T data;
 
+		template<class... Args>
+		node(node* p, node* n, Args&&... args)
+			: prev(p), next(n), data(std::forward<Args>(args)...) {}
+	};
+
+	node* _head;
+	node* _back;
+	size_type _size;
+	Allocator data_alloc;
+public:
+	//构造函数
+	list()
+		: _head(nullptr), _back(nullptr), _size(0), data_alloc() {}
+
+	explicit list(const Allocator& alloc)
+		: _head(nullptr), _back(nullptr), _size(0), data_alloc(alloc) {}
+
+	list(size_type count, const T& value, const Allocator& alloc = Allocator());
+	explicit list(size_type count, const Allocator& alloc = Allocator());
+	template< class InputIt >
+	list(InputIt first, InputIt last,
+		const Allocator& alloc = Allocator());
+	list(const list& other);
+	list(const list& other, const Allocator& alloc);
+	list(list&& other);
+	list(list&& other, const Allocator& alloc);
+	list(std::initializer_list<T> init, const Allocator& alloc = Allocator());
 };
 
 #endif // !LIST_HPP
